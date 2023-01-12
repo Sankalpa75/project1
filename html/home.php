2a4741c4b43d3795151dfde15c12095c88c9f46b
Sankalpa75 <bhattaraisankalpa1@gmail.com>
2023-01-12 21:24:51 +0545

    message we

---
+++ html/home.php
@@ -0,0 +1,11 @@
+
+<div class="one"><img src="images/body.png" class="img-responsive center-block">
+	<div class="container">
+		<div class="row">
+			
+			</div>
+		</div>
+	</div>
+
+
+
---
+++ html/includes/banner.php
@@ -0,0 +1,32 @@
+
+<div id="owl-demo" class="owl-carousel owl-theme">
+
+  <div class="item">
+    <div class="banner-content">
+      <img src="images/b1.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b2.jpg">
+      
+    </div>
+  </div>
+
+   <div class="item">
+    <div class="banner-content">
+      <img src="images/b3.jpg">
+      
+    </div>
+  </div>
+
+
+
+
+
+
+  
+
+</div>
